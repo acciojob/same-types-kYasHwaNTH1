0c536faeb53d7de0a797ff669b869ff35732a519
Yashwanth Kesagoni <kesagoniyashwanth@gmail.com>
2025-09-07 21:41:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
+// function isSameType(value1, value2) {
+//   //your js code here
+// 	if(typeof(value1)===typeof(value2)) return true;
+// 	return false;
+// }
+
+// // do not change the code below.
+// let value1 = prompt("Enter Start of the Range.");
+// let value2 = prompt("Enter End Of the Range.");
+// alert(isSameType(value1, value2));
+
+
+function parseValue(value) {
+  // Try to parse number
+  if (!isNaN(value) && value.trim() !== "") {
+    return Number(value);
+  }
+  // Convert "true"/"false" to boolean
+  if (value.toLowerCase() === "true") return true;
+  if (value.toLowerCase() === "false") return false;
+  // Otherwise keep as string
+  return value;
+}
+
 function isSameType(value1, value2) {
-  //your js code here
-	if(typeof(value1)===typeof(value2)) return true;
-	return false;
+  return typeof value1 === typeof value2;
 }
 
 // do not change the code below.
-let value1 = prompt("Enter Start of the Range.");
-let value2 = prompt("Enter End Of the Range.");
+let value1 = parseValue(prompt("Enter Start of the Range."));
+let value2 = parseValue(prompt("Enter End Of the Range."));
 alert(isSameType(value1, value2));
